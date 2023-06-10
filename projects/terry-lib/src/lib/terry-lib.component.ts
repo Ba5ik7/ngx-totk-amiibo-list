b6bf1ac5167fab25232dd6e930156e7a6a77bc88
Wesley DuSell <dusellfam@gmail.com>
2023-06-10 17:20:56 -0400

    INIT

---
+++ projects/terry-lib/src/lib/terry-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'terry-lib',
+  template: `
+    <p>
+      terry-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TerryLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
