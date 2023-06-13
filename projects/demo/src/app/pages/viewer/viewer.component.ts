b7de128043bdb0aa2433f5353b7b471ddfbf569d
Wesley DuSell <dusellfam@gmail.com>
2023-06-12 22:26:25 -0400

    Create new viewer page

---
+++ projects/demo/src/app/pages/viewer/viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ngx-viewer',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>viewer works!</p>`,
+  styles: [
+  ]
+})
+export class ViewerComponent {}
