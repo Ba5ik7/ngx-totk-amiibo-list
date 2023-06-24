8ef1c7c5b44fe1883b0e8a59254dda2ff48477e1
Wesley DuSell <dusellfam@gmail.com>
2023-06-24 13:17:26 -0400

    Saving point for AR service

--- projects/demo/src/app/pages/viewer/viewer.component.ts
+++ projects/demo/src/app/pages/viewer/viewer.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArService } from '../../services/ar/ar.service';
 
 @Component({
   selector: 'ngx-viewer',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>viewer works!</p>`,
-  styles: [
-  ]
+  template: `<div #container></div>`,
+  styles: []
 })
-export class ViewerComponent {}
+export class ViewerComponent implements OnInit {
+  arService = inject(ArService);
+
+  @ViewChild('container', { static: true }) containerRef!: ElementRef<HTMLElement>;
+
+  ngOnInit() {
+    this.arService.initialize(this.containerRef.nativeElement);
+  }
+}
--- projects/demo/src/app/services/ar/ar.service.ts
+++ projects/demo/src/app/services/ar/ar.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, NgZone } from '@angular/core';
 import * as THREE from 'three';
-import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
+import { THREEx } from '@ar-js-org/ar.js-threejs';
+import { ArToolkitSource } from '@ar-js-org/ar.js-threejs/types/ArToolkitSource';
+import { ArToolkitContext } from '@ar-js-org/ar.js-threejs/types/ArToolkitContext';
+import { ArMarkerControls } from '@ar-js-org/ar.js-threejs/types/ArMarkerControls';
 
 @Injectable({
   providedIn: 'root'
@@ -8,39 +11,97 @@ import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
 export class ArService {
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
-  private camera!: THREE.PerspectiveCamera;
+  private camera!: THREE.Camera;
+  private arToolkitSource!: ArToolkitSource; 
+  private arToolkitContext!: ArToolkitContext; 
+  private arMarker!: ArMarkerControls;
 
   constructor(private ngZone: NgZone) { }
 
   initialize(container: HTMLElement): void {
     this.initThree(container);
-    this.initAr();
+    this.initAr().then(() => {
+
+    });
   }
 
   private initThree(container: HTMLElement): void {
-    // Initialize renderer
-    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true
+    });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(container.offsetWidth, container.offsetHeight);
-    this.renderer.xr.enabled = true;
     container.appendChild(this.renderer.domElement);
-
-    // Initialize scene
-    this.scene = new THREE.Scene();
+  
+    // Set up a perspective camera
+    this.camera = new THREE.PerspectiveCamera(75, container.offsetWidth / container.offsetHeight, 0.1, 1000);
     
-    // Initialize camera
-    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+    this.scene = new THREE.Scene();
+    this.scene.add(this.camera);
+  }  
+
+  private async initAr(): Promise<void> {
+    // Create AR source
+    this.arToolkitSource = new THREEx.ArToolkitSource({
+      sourceType: 'webcam',
+      sourceWidth: 640,
+      sourceHeight: 480,
+    });
+
+    await new Promise<void>(resolve => {
+      this.arToolkitSource.init(() => {
+        resolve();
+      }, () => {
+        console.error('Unable to initialize AR toolkit source.');
+      });
+    });
+
+    console.log('arToolkitSource', this.arToolkitSource);
+    
+    this.arToolkitSource.domElement.addEventListener('canplay', () => {
+      console.log('canplay');
+      this.initARContext();
+    });    
   }
 
-  private initAr(): void {
-    // Initialize AR session
-    document.body.appendChild(ARButton.createButton(this.renderer));
+  private async initARContext(): Promise<void> {
+    // Create AR context
+    this.arToolkitContext = new THREEx.ArToolkitContext({
+      cameraParametersUrl: '/assets/img/camera_para.dat',
+      detectionMode: 'mono'
+    });
+    await new Promise<void>(resolve => {
+      this.arToolkitContext.init(() => {
+        this.camera.projectionMatrix.copy(this.arToolkitContext.getProjectionMatrix());
+        resolve();
+      });
+    });
 
-    // Animate
+    this.initMarker();
+    this.animate();
+  }
+
+
+  private initMarker(): void {
+    console.log(this.arToolkitContext, this.arToolkitContext.arController.camera);
+    
+    // Create marker
+    this.arMarker = new THREEx.ArMarkerControls(this.arToolkitContext, this.arToolkitContext.arController.camera, {
+      type: 'pattern',
+      patternUrl: '/assets/patt.hiro',
+      changeMatrixMode: 'cameraTransformMatrix'
+    });
+  }
+  
+  private animate(): void {
     this.ngZone.runOutsideAngular(() => {
-      this.renderer.setAnimationLoop((timestamp, frame) => {
-        // Handle frame logic
-        this.renderer.render(this.scene, this.camera);
+      this.renderer.setAnimationLoop(() => {
+        if (this.arToolkitSource.ready) {
+          this.arToolkitContext.update(this.arToolkitSource.domElement);
+          this.scene.visible = this.arToolkitContext.arController.camera.visible;
+        }
+        this.renderer.render(this.scene, this.arToolkitContext.arController.camera);
       });
     });
   }
