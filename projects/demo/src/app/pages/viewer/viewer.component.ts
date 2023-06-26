5448eb2157caa5ae5922d1f3c5c2732f85250363
Wesley DuSell <dusellfam@gmail.com>
2023-06-25 21:09:34 -0400

    Testing on mobile needed SSL and

--- projects/demo/src/app/pages/viewer/viewer.component.ts
+++ projects/demo/src/app/pages/viewer/viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArService } from '../../services/ar/ar.service';
 
@@ -7,19 +7,23 @@ import { ArService } from '../../services/ar/ar.service';
   standalone: true,
   imports: [CommonModule],
   template: `<div #container></div><p>{{error}}`,
-  styles: []
+  styles: [],
 })
 export class ViewerComponent implements OnInit, OnDestroy {
   arService = inject(ArService);
+  renderer2 = inject(Renderer2);
   error = this.arService.error;
 
   @ViewChild('container', { static: true }) containerRef!: ElementRef<HTMLElement>;
 
   ngOnInit() {
+    this.renderer2.setStyle(document.body, 'overflow', 'hidden');
     this.arService.initialize(this.containerRef.nativeElement);
   }
 
   ngOnDestroy(): void {
     this.arService.dispose();
+    // need to reset the body overflow to visible
+    this.renderer2.setStyle(document.body, 'overflow', 'visible');
   }
 }
--- projects/demo/src/app/services/ar/ar.service.ts
+++ projects/demo/src/app/services/ar/ar.service.ts
@@ -76,9 +76,9 @@ export class ArService {
       });``
 
       // this.onResize();
-      // setTimeout(() => {
-      //   this.onResize();
-      // }, 2000);
+      setTimeout(() => {
+        this.onResize();
+      }, 2000);
     }, () => { console.log('error') });
 
     this.resizeListener = () => {
@@ -160,7 +160,7 @@ export class ArService {
       // 'assets/img/amiibo_zelda_twilight_princess.glb',
       // called when the resource is loaded
       (gltf) => {
-        gltf.scene.scale.set(30, 30, 30);
+        gltf.scene.scale.set(20, 20, 20);
         gltf.scene.rotation.y = 140;
         this.scene.add(gltf.scene);
       },
